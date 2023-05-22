aea9efa8a2f8f0be1fd622f0c3b14b98a1934d0e
dhruvk-081 <dhruvk081@gmail.com>
2023-05-22 10:33:34 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,15 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    setCounter(counter+1)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Button clicked {counter} times</p>
+        <button onClick={handleClick}>Click me</button>
     </div>
   )
 }
